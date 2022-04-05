4497ae50f4447d0f9d8fe6f86f8f6dd4ab7711cf
Ivo Woltring <i.woltring@anva.nl>
2022-04-05 21:31:05 +0200

    multiplatform centos java 11

--- root/setup/scripts/030-gosu.sh
+++ root/setup/scripts/030-gosu.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-#https://github.com/tianon/gosu/releases
-#grab gosu for easy step-down from root
-GOSU_VERSION=1.12
-dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')";
-curl -s -L -o /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-${dpkgArch}"
-curl -s -L -o /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-${dpkgArch}.asc"
-export GNUPGHOME="$(mktemp -d)"
-gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
-gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
-command -v gpgconf && gpgconf --kill all || :;
-rm -rf "$GNUPGHOME" /usr/local/bin/gosu.asc
-chmod +x /usr/local/bin/gosu
-gosu --version;
-
+$agi gosu
