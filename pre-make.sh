0230a921cf6c326939cf4d9fb4da34300f8a24ec
Ivo Woltring <ivo@ivonet.nl>
2022-05-09 09:24:10 +0200

    small adjustments
    Signed-off-by: Ivo Woltring <ivo@ivonet.nl>

--- pre-make.sh
+++ pre-make.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Build custom login extension..."
-cd ivonet-guacamole-custom-login
-mvn clean package clean
-cd ..
+if [ ! -f "root/etc/guacamole/extensions/ivonet-guacamole-custom-login.jar" ]; then
+  echo "Build custom login extension..."
+  cd ivonet-guacamole-custom-login
+  mvn clean package clean
+  cd ..
+fi
 
-echo "Build auto login extension..."
-cd ivonet-guacamole-docker-auto-login
-mvn clean package clean
-cd ..
+if [ ! -f "root/etc/guacamole/extensions/ivonet-guacamole-docker-auto-login.jar" ]; then
+  echo "Build auto login extension..."
+  cd ivonet-guacamole-docker-auto-login
+  mvn clean package clean
+  cd ..
+fi
